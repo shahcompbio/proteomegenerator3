43f59adbed9507dd201349bafe9890483a4888ef
apsteinberg <asherps@gmail.com>
2026-02-05 17:50:24 -0500

    revert to filtering on full-length counts

--- bin/bambu_filter.R
+++ bin/bambu_filter.R
@@ -2,27 +2,27 @@
 library(bambu)
 library(optparse)
 ## load in commandline args
-option_list = list(
+option_list <- list(
   make_option("--se", type="character", default=NULL,
               help="bambu read classes", metavar="character"),
   make_option("--merge", type="logical", default=FALSE,
               help="if multisample merge was performed", metavar="logical"),
-  make_option("--min_cpm", type="double", default=1,
-              help="minimum cpm for detected transcript")
+  make_option("--min_cts", type="double", default=1,
+              help="min full-length reads for detected transcript")
 );
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 # load summarized experiment object
 load(opt$se)
-# filter on cpms
-min_cpm <- opt$min_cpm
+# filter on full-length read counts
+min_cts <- opt$min_cts
 if (opt$merge) {
-  CPM <- assays(se)$CPM
-  rows_satisfying_conditions <- apply(CPM, 1, function(row) any(row >= min_cpm))
+  fullLengthCounts <- assays(se)$fullLengthCounts
+  rows_satisfying_conditions <- apply(fullLengthCounts, 1, function(row) any(row >= min_cts))
   # Subset the SummarizedExperiment object based on the condition
   se.detected <- se[rows_satisfying_conditions, ]
 } else {
-  se.detected <- se[assays(se)$CPM >= min_cpm,]
+  se.detected <- se[assays(se)$fullLengthCounts >= min_cts,]
 }
 detectgtf <- rowRanges(se.detected)
 writeToGTF(detectgtf, "detected_transcripts.gtf")
